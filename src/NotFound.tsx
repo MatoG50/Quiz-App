fa58c6587bce5491fd107e0019e1063a185981ae
MatoG50 <matog50@gmail.com>
2024-03-12 20:18:07 +0300

    Not found page

---
+++ src/NotFound.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@chakra-ui/react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <Text>Page not Found</Text>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Quiz from './Quiz';
+import NotFound from './NotFound';
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/quiz/:id/question/:questionId', element: <Quiz /> },
+  { path: '*', element: <NotFound /> },
 ]);
 
 export default router;
